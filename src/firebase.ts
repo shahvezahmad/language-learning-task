157a8f8d75846cd8332a2c69310a0f7c0e0309e2
Shahvez Ahmad <shahvez.ahmad2002@gmail.com>
2023-09-22 23:31:28 +0530

    firebase setup

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBRc2xVOO9l4XW0v3pxLClnanwqFLweqiw",
+  authDomain: "language-learn-4bc69.firebaseapp.com",
+  databaseURL: "https://language-learn-4bc69-default-rtdb.firebaseio.com",
+  projectId: "language-learn-4bc69",
+  storageBucket: "language-learn-4bc69.appspot.com",
+  messagingSenderId: "103501559212",
+  appId: "1:103501559212:web:e523b14ee4cc7e20e6f609",
+  measurementId: "G-GN4LYWLKCS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
